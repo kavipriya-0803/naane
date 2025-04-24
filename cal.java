14cbae791334115f2a277ee288af8eb951b880a8
kavipriya-0803 <231001085@rajalakshmi.edu.in>
2025-04-24 22:17:14 +0530

    Created calculator

---
+++ cal.java
@@ -0,0 +1,23 @@
+public class kp{
+  public static void main(String[] args){
+    Scanner sc= new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    System.out.println("1.add 2.sub 3.mul 4.div 5.mod");
+    System.out.print("Enter yout option: ");
+    int n=sc.nextInt();
+    if(n==1){
+      System.out.println(a+b);
+    }
+    else if(n==2){
+      System.out.println(a-b);
+    }
+    else if(n==3){
+      System.out.println(a*b);
+    }
+    else if(n==4){
+      System.out.println(a/b);
+    }
+    else{
+      System.out.println(a%b);
+    }
